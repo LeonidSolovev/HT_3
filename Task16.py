13e0d69519574b37dc5ef4546e13de6ba92cf02a
Leonid Solovev <ls3112@yandex.ru>
2023-06-24 16:40:47 +0300

    Task 16

--- Task16.py
+++ Task16.py
@@ -3,3 +3,17 @@
 # натуральное число N – количество элементов в массиве. В последующих
 # строках записаны N целых чисел Ai
 # . Последняя строка содержит число X
+
+l = int(input("Введите длину списка: "))
+list_1 = []
+i = 0
+while len(list_1) <l:
+    list_1.append(int(input("Введите число: ")))
+    i+=1
+print(list_1)
+count =0
+num = int(input("Введите искомое число: "))
+for j in range(len(list_1)):
+    if num==list_1[j]:
+        count +=1
+print(count)
